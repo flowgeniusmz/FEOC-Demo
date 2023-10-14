0122710397d2108a62be941e742ab0b693b178d6
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-10-13 22:12:48 -0500

    Create pagesetup.py

---
+++ pagesetup.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import streamlit.components.v1 as c
+from streamlit_elements import elements, mui, html
+
+
+def set_title(varTitle, varSubtitle):
+        st.markdown(f"""# {varTitle} <span style=color:#7ab3ba><font size=5>{varSubtitle}</font></span>""",unsafe_allow_html=True)
