82a0f492ee9e429c1890759cf0ef178da3ee2844
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-10-14 09:42:01 -0500

    Update pagesetup.py
    update to match green look

--- pagesetup.py
+++ pagesetup.py
@@ -4,4 +4,4 @@ from streamlit_elements import elements, mui, html
 
 
 def set_title(varTitle, varSubtitle):
-        st.markdown(f"""# {varTitle} <span style=color:#7ab3ba><font size=5>{varSubtitle}</font></span>""",unsafe_allow_html=True)
+        st.markdown(f"""# {varTitle} <span style=color:#2D7629><font size=5>{varSubtitle}</font></span>""",unsafe_allow_html=True)
