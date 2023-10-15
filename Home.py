5184b6a7f71558af3fe9530b997300ea8ffc35c0
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-10-14 21:26:29 -0500

    Update Home.py
    tab change (1 of 2)

--- Home.py
+++ Home.py
@@ -2,6 +2,13 @@ import streamlit as st
 from login import get_loginform
 from pagesetup import set_title
 
+#update the tab
+def load_image(Image_name):
+    img= Image.open(Image_name)
+    return img
+img= load_image('')
+st.set_page_config(page_title='FEOC FG', page_icon=img)
+
 
 if 'authenticated' not in st.session_state:
     get_loginform()
