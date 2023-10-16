30a79e07a5d6be3a00aad32cd5d6415772af9e82
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-10-15 22:03:21 -0500

    Update 9_Alternate_Home.py

--- pages/9_Alternate_Home.py
+++ pages/9_Alternate_Home.py
@@ -1,32 +1,64 @@
 import streamlit as st
-from login import get_loginform
 from pagesetup import set_title
 
-if 'authenticated' not in st.session_state:
-    get_loginform()
-else:
-    set_title("Faulkner Emission Offset Certificate Program", "Home")
-    st.divider()
-    main_container = st.container()
-    with main_container:
-        exp1 = st.expander("Overview", expanded=True)
-        with exp1:
-            st.header("Welcome to the Faulkner Emission Solutions Platform!")
+st.set_page_config(layout="wide")
+set_title("FEOC", "Central Performance Panel")
+
+# Display progress bar with the emissions reduced
+progress = st.progress(0.5)
+st.caption("Total Emissions Reduced")
+st.divider()
+
+# Certificate Information
+with st.expander("Certificate Information", expanded=True):
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        st.markdown("#### Details")
+        col1a, col1b, col1c = st.columns(3)
+        
+        with col1a:
+            st.markdown("**Project:** Carbon Offset for Refinery")
+            st.markdown("**FEOC ID:** FEOC-001")
+            st.markdown("**Start Date:** 01/01/2023")
             
-            st.write("We are thrilled to have you join our mission. Whether you're an Emitter, a Provider, a Purchaser, or simply a champion for the environment, this platform aims to unify our collective efforts towards a greener planet.")
+        with col1b:
+            st.markdown("**Emitter:** Petroleum Refinery")
+            st.markdown("**Provider:** GismoPower")
+            st.markdown("**Exp. Date:** 12/31/2030")
             
-            st.markdown("**Emitters**, you are the linchpin of emission reduction, driving us towards a sustainable future. Your investments empower Providers to innovate and bring forth Green Solutions that battle against emissions.")
-            
-            st.markdown("**Providers**, with your groundbreaking solutions, you are lighting the path to a brighter, cleaner future. By collaborating with Emitters, we are making strides in environmental conservation.")
-            
-            st.markdown("**Purchasers**, by choosing to back reduced emission products, you set a commendable standard. Every purchase you make takes us one step closer to a cleaner, better world.")
-            
-            st.markdown("Introducing the **Faulkner Certificates** - our benchmark for emission offset. These certificates not only validate and track environmental contributions but, with the aid of state-of-the-art AI technology, ensure their authenticity, accuracy, and completeness.")
-            
-            st.subheader("Harness the power of cutting-edge AI technology.")
-            st.write("Our AI continuously refines and validates data, ensuring accuracy and credibility at every step.")
-            
-            st.subheader("To our vibrant user community")
-            st.write("Your interactions and inputs amplify the effectiveness of our platform. Each one of you is a cog in this grand machinery combating climate change. Together, let's pave the way for a sustainable, verdant future for the next generation.")
-            
-            st.write("Jump in, explore, and be the change you wish to see in the world!")
+        with col1c:
+            st.markdown("**Purchaser:** Commercial Airline")
+            st.markdown("**AAA-Rating:** Rating Value")
+    
+    with col2:
+        st.markdown("#### Notifications and Alerts")
+        st.text_area(
+            label="**Alerts:**",
+            value="""**Alert 1:** 10/01/2023 Financial transaction initiated by John Doe  
+**Alert 2:** 10/01/2023 Financial transaction initiated by John Doe  
+**Alert 3:** 10/01/2023 Financial transaction initiated by John Doe  
+**Alert 4:** 10/01/2023 Financial transaction initiated by John Doe """,
+            disabled=True)
+
+# Performance
+with st.expander("Performance", expanded=True):
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        st.markdown("#### Financial")
+        st.bar_chart({"Operational Profits (in metric tons CO₂e)": [400000], "Net-Zero Fuel Sold (in metric tons)": [1000000]})
+        st.markdown("**Total Net-Zero Tickets Sold:** 10,000,000 tickets")
+        st.markdown("**Current OC Value:** $2,504,543,232")
+    
+    with col2:
+        st.markdown("#### Carbon Reduction")
+        data = {"Annual Offset (Committed)": 400000, "Annual Offset (Actual)": 300000}
+        st.bar_chart(data)
+        st.markdown("**Variance:** 100,000 (25.00%) metric tons CO₂e")
+    
+    with col3:
+        st.markdown("#### Validation and Verification")
+        st.warning("9/21/2023: Please check data compliance")
+        st.success("9/20/2023: Data validation successfully completed; no errors")
+        st.info("9/15/2023: Forward-selling activity initiated (100,000 tons fuel)")
