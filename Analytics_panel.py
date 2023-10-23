3e80c80ec0cdf636aef288298e2a718f95ea6d86
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-10-22 21:51:29 -0500

    Update Analytics_panel.py

--- Analytics_panel.py
+++ Analytics_panel.py
@@ -15,40 +15,40 @@ def display_Analytics_panel():
     with col03:
         st.metric("Project Financing", "$5,000,000", "-$500,000")
 
-    # Financial Analytics
-    st.header("Financial Analytics")
-    col11, col12, col13 = st.columns(3)
-    with col11:
-        st.subheader("Carbon Offset Revenue")
-        chart_data11 = pd.DataFrame({
-            'Months': list(range(1, 13)),
-            'Revenue': np.random.randint(100000, 150000, 12)
-        })
-        st.line_chart(chart_data11.set_index('Months'))
-
-    with col12:
-        st.subheader("Carbon Reduction vs Commitment")
-        chart_data12 = pd.DataFrame({
-            'Months': list(range(1, 13)),
-            'Actual Reduction': np.random.randint(100000, 130000, 12),
-            'Committed Reduction': [125000] * 12
-        })
-        st.line_chart(chart_data12.set_index('Months')
-
-    # with col13:
-    #     st.subheader("Monthly Financing vs Returns")
-    #     chart_data13 = pd.DataFrame({
+    # # Financial Analytics
+    # st.header("Financial Analytics")
+    # col11, col12, col13 = st.columns(3)
+    # with col11:
+    #     st.subheader("Carbon Offset Revenue")
+    #     chart_data11 = pd.DataFrame({
     #         'Months': list(range(1, 13)),
-    #         'Financing': np.random.randint(300000, 500000, 12),
-    #         'Returns': np.random.randint(250000, 450000, 12)
+    #         'Revenue': np.random.randint(100000, 150000, 12)
     #     })
-    #     st.bar_chart(chart_data13.set_index('Months'))
+    #     st.line_chart(chart_data11.set_index('Months'))
 
-    # # Live Sensor Data
-    # # st.header("Live Sensor Data Details")
-    # # st.write("In our environmental monitoring project, data was collected by ...")
-    # # places = pd.read_csv('.localdata/refinery_sensor_data_test_extended_values.csv')
-    # # st.map(places)
+    # with col12:
+    #     st.subheader("Carbon Reduction vs Commitment")
+    #     chart_data12 = pd.DataFrame({
+    #         'Months': list(range(1, 13)),
+    #         'Actual Reduction': np.random.randint(100000, 130000, 12),
+    #         'Committed Reduction': [125000] * 12
+    #     })
+    #     st.line_chart(chart_data12.set_index('Months')
+
+    with col13:
+        st.subheader("Monthly Financing vs Returns")
+        chart_data13 = pd.DataFrame({
+            'Months': list(range(1, 13)),
+            'Financing': np.random.randint(300000, 500000, 12),
+            'Returns': np.random.randint(250000, 450000, 12)
+        })
+        st.bar_chart(chart_data13.set_index('Months'))
+
+    # Live Sensor Data
+    # st.header("Live Sensor Data Details")
+    # st.write("In our environmental monitoring project, data was collected by ...")
+    # places = pd.read_csv('.localdata/refinery_sensor_data_test_extended_values.csv')
+    # st.map(places)
 
     # # Operational Analytics
     # st.header("Operational Analytics")
