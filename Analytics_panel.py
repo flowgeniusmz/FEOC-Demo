963d715cfb6267391d0881ab0cc4828c9f997907
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-10-22 21:57:12 -0500

    Update Analytics_panel.py

--- Analytics_panel.py
+++ Analytics_panel.py
@@ -5,44 +5,44 @@ import numpy as np
 def display_Analytics_panel():
     st.title("FEOC Analytics Panel")
 
-    # Key Metrics
-    # st.header("Key Metrics")
-    # col01, col02, col03 = st.columns(3)
-    # with co101:
-    #     st.metric("Carbon Reduction Commitment", "1,500,000 Tons", "+250,000")
-    # with col02:
-    #     st.metric("Net Zero Fuel Sales", "$1,500,000", "+$300,000")
-    # with col03:
-    #     st.metric("Project Financing", "$5,000,000", "-$500,000")
+    Key Metrics
+    st.header("Key Metrics")
+    col01, col02, col03 = st.columns(3)
+    with co101:
+        st.metric("Carbon Reduction Commitment", "1,500,000 Tons", "+250,000")
+    with col02:
+        st.metric("Net Zero Fuel Sales", "$1,500,000", "+$300,000")
+    with col03:
+        st.metric("Project Financing", "$5,000,000", "-$500,000")
 
-    # # Financial Analytics
-    # st.header("Financial Analytics")
-    # col11, col12, col13 = st.columns(3)
-    # with col11:
-    #     st.subheader("Carbon Offset Revenue")
-    #     chart_data11 = pd.DataFrame({
-    #         'Months': list(range(1, 13)),
-    #         'Revenue': np.random.randint(100000, 150000, 12)
-    #     })
-    #     st.line_chart(chart_data11.set_index('Months'))
+    # Financial Analytics
+    st.header("Financial Analytics")
+    col11, col12, col13 = st.columns(3)
+    with col11:
+        st.subheader("Carbon Offset Revenue")
+        chart_data11 = pd.DataFrame({
+            'Months': list(range(1, 13)),
+            'Revenue': np.random.randint(100000, 150000, 12)
+        })
+        st.line_chart(chart_data11.set_index('Months'))
 
-    # with col12:
-    #     st.subheader("Carbon Reduction vs Commitment")
-    #     chart_data12 = pd.DataFrame({
-    #         'Months': list(range(1, 13)),
-    #         'Actual Reduction': np.random.randint(100000, 130000, 12),
-    #         'Committed Reduction': [125000] * 12
-    #     })
-    #     st.line_chart(chart_data12.set_index('Months')
+    with col12:
+        st.subheader("Carbon Reduction vs Commitment")
+        chart_data12 = pd.DataFrame({
+            'Months': list(range(1, 13)),
+            'Actual Reduction': np.random.randint(100000, 130000, 12),
+            'Committed Reduction': [125000] * 12
+        })
+        st.line_chart(chart_data12.set_index('Months')
 
-    # with col13:
-    #     st.subheader("Monthly Financing vs Returns")
-    #     chart_data13 = pd.DataFrame({
-    #         'Months': list(range(1, 13)),
-    #         'Financing': np.random.randint(300000, 500000, 12),
-    #         'Returns': np.random.randint(250000, 450000, 12)
-    #     })
-    #     st.bar_chart(chart_data13.set_index('Months'))
+    with col13:
+        st.subheader("Monthly Financing vs Returns")
+        chart_data13 = pd.DataFrame({
+            'Months': list(range(1, 13)),
+            'Financing': np.random.randint(300000, 500000, 12),
+            'Returns': np.random.randint(250000, 450000, 12)
+        })
+        st.bar_chart(chart_data13.set_index('Months'))
 
     # Live Sensor Data
     # st.header("Live Sensor Data Details")
